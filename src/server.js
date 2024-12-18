eb1eb0016ca37fde950f09d7bdf9eac83e1c60ba
th4nh-phat09 <thanphat354@gmail.com>
2024-12-18 11:14:48 +0700

    add template engine

--- src/server.js
+++ src/server.js
@@ -1,10 +1,36 @@
 import express from "express";
+import morgan from "morgan";
+import { engine  } from "express-handlebars"
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// Init __dirname
+const __filename = fileURLToPath(import.meta.url); // Lấy đường dẫn file hiện tại
+const __dirname = path.dirname(__filename); // Lấy thư mục chứa file hiện tại src
+
+console.log(__dirname)
+
 
 const app =express();
 const port =3000;
 
+// Middleware for TEMPLATE ENGINE
+app.engine('hbs', engine ({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../resources/views'));
+
+//console.log(path.join(__dirname));
+
+// Middleware for logging requests
+app.use(morgan('combined'))
 app.get("/",(req,res)=>{
-    res.send("Hello, world!");
+    res.render('home');
+})
+
+app.get("/news",(req,res)=>{
+    res.render('new');
 })
 
 app.listen(port, () => {
